71c9b1a352f1b9492f05a29399ffd45e5fb90461
coolzai <coolzai@users.noreply.github.com>
2016-05-23 00:54:51 -0700

    add US-letter, US-legal paper sizes (#1)
    * add US-letter, US-legal paper sizes

--- src/tragediy.cpp
+++ src/tragediy.cpp
@@ -136,6 +136,26 @@ auto setPaperSize(const BoundingBox &bbTrack) -> std::tuple<double, double>
 		paperWidth = 210.0;
 		paperHeight = 297.0;
 	}
+	else if (tileSize_ == "letter-landscape")
+	{
+		paperWidth = 279.4;
+		paperHeight = 215.9;
+	}
+	else if (tileSize_ == "letter-portrait")
+	{
+		paperWidth = 215.9;
+		paperHeight = 279.4;
+	}
+	else if (tileSize_ == "legal-landscape")
+	{
+		paperWidth = 355.6;
+		paperHeight = 215.9;
+	}
+	else if (tileSize_ == "legal-portrait")
+	{
+		paperWidth = 215.9;
+		paperHeight = 355.6;
+	}
 	else
 	{
 		paperWidth = bbTrack.xMax_ - bbTrack.xMin_;
@@ -279,7 +299,7 @@ int main(int argc, char *argv[])
 	{
 		tileSize_ = vm["size"].as<std::string>();
 
-		if (tileSize_ != "a3-landscape" && tileSize_ != "a4-landscape" && tileSize_ != "a3-portrait" && tileSize_ != "a4-portrait" && tileSize_ != "full")
+		if (tileSize_ != "a3-landscape" && tileSize_ != "a4-landscape" && tileSize_ != "a3-portrait" && tileSize_ != "a4-portrait" && tileSize_ != "letter-landscape" && tileSize_ != "legal-landscape" && tileSize_ != "letter-portrait" && tileSize_ != "legal-portrait" && tileSize_ != "full")
 		{
 			std::cout << "Invalid tile size '" << tileSize_ << "'." << std::endl;
 			std::exit(EXIT_FAILURE);
